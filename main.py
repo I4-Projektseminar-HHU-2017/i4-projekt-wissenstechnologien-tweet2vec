ae9e745948ec9a2a8924b05d97fbad0f9bc536c3
Matthias Krüger <matthias.krueger@famsik.de>
2017-06-08 17:40:09 +0200

    script: implement custom wrapper object for tweepy tweets which can be used to access hashtags, mentions and tweet message.
    t = Tweet(i) // i is the tweepy tweet object
    t.message()
    t.emojis()
    t.hashtags()
    t.mentions()

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import tweepy # http://docs.tweepy.org/en/v3.5.0/getting_started.html
 import sys # cheap arg parse
 import emoji # pip3  install --user emoji  //  https://github.com/carpedm20/emoji/
 
+
+
+
 def tweepy_getAPI(): # log in via auth token and return api
 	KEY = my_config.consumer_key
 	SECRET = my_config.consumer_secret
@@ -16,30 +19,35 @@ def tweepy_getAPI(): # log in via auth token and return api
 	api = tweepy.API(auth)
 	return api
 
-def extract_emojis(string): # return list of emojis
-	emojis=[]
-# emoji.UNICODE_EMOJI taken from https://stackoverflow.com/questions/43146528/how-to-extract-all-the-emojis-from-text
-	for char in string: # walk text char by char
-		if (char in emoji.UNICODE_EMOJI):  # char is recognized as emoji
-			emojis.append(char)
-	return emojis # return list of emojis
+class Tweet:
+	def __init__(self,tweepyRef): # reference to tweet obj by tweepy
+		self.ref = tweepyRef
+		self.text = tweepyRef.text
 
+	def emojis(self): # return list of emojis
+		emojis=[]
+		# emoji.UNICODE_EMOJI taken from https://stackoverflow.com/questions/43146528/how-to-extract-all-the-emojis-from-text
+		for char in self.text: # walk text char by char
+			if (char in emoji.UNICODE_EMOJI):  # char is recognized as emoji
+				emojis.append(char)
+		return emojis # return list of emojis
 
-def extract_hashtags(string): # return list of hashtags
-	hashtags=[]
-	for word in string.split(): # split line into words
-		if (word[0] == "#") and (len(word) > 1):  # word starts with hashtag and is longer then one char  
-			hashtags.append(word)
-	return hashtags
+	def hashtags(self): # return list of hashtags
+		hashtags=[]
+		for word in self.text.split(): # split line into words
+			if (word[0] == "#") and (len(word) > 1):  # word starts with hashtag and is longer then one char
+				hashtags.append(word)
+		return hashtags
 
+	def mentions(self): # return list of hashtags
+		mentions=[]
+		for word in self.text.split(): # split line into words
+			if (word[0] == "@") and (len(word) > 1):  # word starts with mention and is longer then one char
+				mentions.append(word)
+		return mentions
 
-def extract_mentions(string): # return list of hashtags
-	mentions=[]
-	for word in string.split(): # split line into words
-		if (word[0] == "@") and (len(word) > 1):  # word starts with mention and is longer then one char  
-			mentions.append(word)
-	return mentions
-
+	def message(self):
+		return self.text
 
 API = tweepy_getAPI()
 
@@ -63,14 +71,14 @@ user_id = API.get_user(ACC).screen_name
 last_tweets = API.user_timeline(user_id, count = 5) # count is the number of tweets to retrieve
 
 for i in last_tweets:
-	TEXT = i.text
+	t = Tweet(i) # this creates our own custom wrapper object
 	print("Tweet:")
-	print(TEXT) # tweet.text
-	print("")
-	print("Emojis: " + str(extract_emojis(TEXT)))
-	print("Hashtags: " + str(extract_hashtags(TEXT)))
-	print("Mentions: " + str(extract_mentions(TEXT)))
+	print(t.message())
+	print(t.emojis())
+	print(t.hashtags())
+	print(t.mentions())
 
+	print("")
 
 	print("\n\n\n")
 
